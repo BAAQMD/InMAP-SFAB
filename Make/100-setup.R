6c4f71cad7990f8ed30a0fb34669087df558e24e
David Holstius <david.holstius@gmail.com>
2022-02-17 09:48:36 -0800

    tidy constants

--- Make/100-setup.R
+++ Make/100-setup.R
@@ -18,9 +18,10 @@ ISRM_FULL_CELL_COUNT <- 52411
 ISRM_CA_CELL_COUNT <- 9001
 
 # The full ISRM (about 166 GB on disk)
-ISRM_full_nc_url <- "https://zenodo.org/record/2589760/files/isrm_v1.2.1.zip?download=1"
-ISRM_full_nc_path <- here::here("Data", "isrm_v1.2.1.ncf")
+ISRM_FULL_NC_URL <- "https://zenodo.org/record/2589760/files/isrm_v1.2.1.zip?download=1"
+ISRM_FULL_NC_PATH <- here::here("Data", "isrm_v1.2.1.ncf")
 
-# The California extraction, via https://drive.google.com/drive/folders/1WmLRz7iWo2MjtSikgHEig7M0NvK2sOns
-#ISRM_CA_nc_url <- "https://drive.google.com/u/0/uc?id=1v__TPKJeiL8Q4zLz8Dyx5HcWSqR2NDx4&export=download&confirm=t"
-ISRM_CA_nc_path <- here::here("Data", "ca_isrm.ncf")
+# The California "hi-res" prototype,
+# via https://drive.google.com/drive/folders/1WmLRz7iWo2MjtSikgHEig7M0NvK2sOns
+ISRM_CA_NC_URL <- "https://drive.google.com/u/0/uc?id=1v__TPKJeiL8Q4zLz8Dyx5HcWSqR2NDx4&export=download&confirm=t"
+ISRM_CA_NC_PATH <- here::here("Data", "ca_isrm.ncf")
