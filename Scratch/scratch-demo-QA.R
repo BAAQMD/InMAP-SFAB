6011d474c82e72b1e872ec5d0cb1d3b8ca9a19bf
David Holstius <david.holstius@gmail.com>
2022-03-31 15:50:18 -0700

    Create scratch-demo-QA.R

---
+++ Scratch/scratch-demo-QA.R
@@ -0,0 +1,19 @@
+with(SFAB_ISRM_demo_data, {
+  testthat::expect_equal(Area, PM25_TOT / TotalPM_area, tol = 0.001)
+  testthat::expect_equal(White_per, White / Total)
+  testthat::expect_equal(DeathsK, DeathsK_White + DeathsK_Black + DeathsK_Asian + DeathsK_Hispanic)
+  #testthat::expect_equal(Total, White + Black + NatAmer + Asian + PcIsl + Other + Multi + Hispanic)
+})
+
+SFAB_ISRM_demo_data %>%
+  mutate(x = White + Black + NatAmer + Asian + PcIsl + Other + Multi) %>%
+  drop_units() %>%
+  gg_scatterplot(
+    aes(x, Total))
+
+SFAB_ISRM_demo_data %>%
+  mutate(x = DeathsK_White + DeathsK_Black + DeathsK_Asian + DeathsK_Hispanic) %>%
+  drop_units() %>%
+  gg_scatterplot(
+    aes(x, DeathsK))
+
