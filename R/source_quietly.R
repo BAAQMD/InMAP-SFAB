f9fed0e7fab7a3d6c2c41fb7202a6a77a4a25489
David Holstius <david.holstius@gmail.com>
2022-03-31 15:49:25 -0700

    fix source_quietly()

--- R/source_quietly.R
+++ R/source_quietly.R
@@ -4,8 +4,8 @@ source_quietly <- function (
   pattern = regex("\\.R$", ignore_case = TRUE)
 ) {
   if (fs::is_dir(path)) {
-    paths <- fs::dir_ls(path, recurse = recurse, pattern = pattern)
-    purrr::walk(paths, source_quietly)
+    paths <- fs::dir_ls(path, recurse = recurse, regexp = pattern)
+    purrr::walk(paths, source_quietly, recurse = FALSE, pattern = pattern)
   } else {
     message("Sourcing ", fs::path_rel(path, here::here()))
     invisible(purrr::quietly(source)(path))
