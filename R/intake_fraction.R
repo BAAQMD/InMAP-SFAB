292f74468a80af1eac2b6f40bdc5101e006ddc00
David Holstius <david.holstius@gmail.com>
2022-04-05 08:53:33 -0700

    Create intake_fraction.R

---
+++ R/intake_fraction.R
@@ -0,0 +1,11 @@
+intake_fraction <- function (
+  conc_qty,
+  ems_qty,
+  breathing_rate = set_units(14.5, "m^3/d"),
+  unit = "ppt"
+) {
+  conc_qty <- set_units(conc_qty, "ug/m^3") # if not supplied
+  ems_qty <- set_units(ems_qty, "ton/yr")   # if not supplied
+  iF <- conc_qty * breathing_rate / ems_qty
+  return(set_units(iF, unit, mode = "character"))
+}
