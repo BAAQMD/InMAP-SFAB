326458ed07b7766331c7fc7d7b90835be77e9e2f
David Holstius <david.holstius@gmail.com>
2022-04-22 10:12:23 -0700

    don't assume units on input to intake_fraction()

--- R/intake_fraction.R
+++ R/intake_fraction.R
@@ -5,8 +5,8 @@ intake_fraction <- function (
   breathing_rate = set_units(14.5, "m^3/d/person"),
   unit = "ppm"
 ) {
-  conc_qty <- set_units(conc_qty, "ug/m^3") # if not supplied
-  ems_qty <- set_units(ems_qty, "ton/yr")   # if not supplied
+  #conc_qty <- set_units(conc_qty, "ug/m^3") # if not supplied
+  #ems_qty <- set_units(ems_qty, "ton/yr")   # if not supplied
   iF <- pop_qty * conc_qty * breathing_rate / ems_qty
   return(set_units(iF, unit, mode = "character"))
 }
