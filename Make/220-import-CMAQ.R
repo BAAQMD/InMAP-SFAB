6081d4500020f776e7626178de5d164035145cb0
David Holstius <david.holstius@gmail.com>
2022-04-25 09:37:05 -0700

    Update 220-import-CMAQ.R

--- Make/220-import-CMAQ.R
+++ Make/220-import-CMAQ.R
@@ -1,16 +1,24 @@
+#'----------------------------------------------------------------------
+#'
+#' Import BAAQMD "Lambert Conformal Conic" (LCC) 1km grid definition
+#' from `BAAQMD_1km_toxic_164X224.zip`.
+#'
+#' Then, re-export to `Build/Geodata/` in GeoJSON and TopoJSON formats.
+#'
+#'----------------------------------------------------------------------
 
-CMAQ_LCC_1km_shp <-
+CMAQ_LCC_1km_grid_shp <-
   data_path(
     "BAAQMD",
     "CMAQ-LCC-1km-grid",
     "BAAQMD_1km_toxic_164X224.zip") %>%
   shptools::read_shp()
 
-CMAQ_LCC_1km_geodata <-
-  CMAQ_LCC_1km_shp %>%
-  select(
-    CMAQ_col = I,
-    CMAQ_row = J)
+CMAQ_LCC_1km_grid_geodata <-
+  CMAQ_LCC_1km_grid_shp %>%
+  transmute(
+    CMAQ_col = as.integer(I),
+    CMAQ_row = as.integer(J))
 
 local({
 
@@ -18,11 +26,11 @@ local({
   topojson_path <- fs::path_ext_set(geojson_path, "topojson")
 
   write_geojson(
-    CMAQ_LCC_1km_geodata,
+    CMAQ_LCC_1km_grid_geodata,
     geojson_path)
 
   write_topojson(
-    CMAQ_LCC_1km_geodata,
+    CMAQ_LCC_1km_grid_geodata,
     topojson_path)
 
 })
