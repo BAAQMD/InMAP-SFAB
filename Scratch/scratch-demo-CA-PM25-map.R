e546630e64ce53373236d5fb041384f508b6f6c7
David Holstius <david.holstius@gmail.com>
2022-04-26 13:26:31 -0700

    create scratch-demo-CA-PM25-map

---
+++ Scratch/scratch-demo-CA-PM25-map.R
@@ -0,0 +1,109 @@
+require_data(CA_ISRM_SFAB_cell_geodata)
+
+#'----------------------------------------------------------------------
+#'
+#' Map of total PM2.5, according to the CA ISRM.
+#'
+#'----------------------------------------------------------------------
+
+fig1_object <- local({
+
+  map_geodata <-
+    CA_ISRM_SFAB_cell_geodata
+
+  map_var <-
+    "BaselineTotalPM25"
+
+  gg_map_SFAB() +
+    geom_sf(
+      aes_string(fill = map_var),
+      color = NA,
+      data = map_geodata) +
+    geom_SFAB_outline(
+      color = "black") +
+    scale_extent(
+      st_extent(SFAB_boundary)) +
+    scale_fill_sepia(
+      limits = c(0, 20)) +
+    theme(
+      legend.position = "top") +
+    annotate_scalebar() +
+    labs(
+      title = str_glue(
+        "CA ISRM"),
+      subtitle = str_glue(
+        "Basis: {comment(map_geodata)}",
+        "Mapped values are extracted from netCDF and joined to shapefile.",
+        .sep = "\n"),
+      caption = str_draft())
+
+})
+
+#'----------------------------------------------------------------------
+#'
+#' Map of total PM2.5, according to the "demo" dataset.
+#'
+#' - WARNING: sectors may be missing.
+#' - See https://github.com/BAAQMD/InMAP-SFAB/issues/1#issuecomment-1105453126
+#'
+#'----------------------------------------------------------------------
+
+fig2_object <- local({
+
+  map_geodata <-
+    SFAB_ISRM_demo_conc_data %>%
+    sum_concentration_by(
+      pol_abbr,
+      any_of(ISRM_ID_VARS)) %>%
+    spread(
+      pol_abbr,
+      conc_qty) %>%
+    inner_join(
+      US_ISRM_SFAB_cell_geometries,
+      .,
+      by = intersect(names(.), ISRM_ID_VARS)) %>%
+    set_comment(
+      comment(SFAB_ISRM_demo_conc_data))
+
+  comment(map_geodata)
+  map_var <-
+    "TotalPM25"
+
+  gg_map_SFAB() +
+    geom_sf(
+      aes_string(fill = map_var),
+      color = NA,
+      data = drop_units(map_geodata)) +
+    geom_SFAB_outline(
+      color = "black") +
+    scale_fill_sepia(
+      limits = c(0, 20)) +
+    scale_extent(
+      st_extent(SFAB_boundary)) +
+    theme(
+      legend.position = "top") +
+    annotate_scalebar() +
+    labs(
+      title = str_glue(
+        "Demo"),
+      subtitle = str_glue(
+        "Basis: {comment(map_geodata)}",
+        "CSV is split by Sector; mapped values are sums, by cell, across Sector.",
+        .sep = "\n"),
+      caption = str_draft())
+
+})
+
+#'----------------------------------------------------------------------
+#'
+#' Paste both maps side-by-side
+#'
+#'----------------------------------------------------------------------
+
+library(patchwork)
+
+ggtools::write_png(
+  (fig1_object + fig2_object),
+  build_path("Demo", "scratch-demo-CA-PM25-map.png"),
+  width = 12,
+  height = 12)
